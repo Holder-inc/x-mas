5c2beca1f25937d104333bec8125a1ff85a0389b
swimmingkiim <54804279+swimmingkiim@users.noreply.github.com>
2022-12-22 17:49:27 +0900

    Update with banner url (#5)

--- src/components/BelowBanner/BelowBanner.svelte
+++ src/components/BelowBanner/BelowBanner.svelte
@@ -1,4 +1,6 @@
-<img src="banner/below_banner.png" alt="건강한 감정소통커뮤니티, 감정분리수거" />
+<a href="https://emotion-recycle.oopy.io/?utm_source=x-mas&utm_medium=e-card&utm_campaign=viral">
+	<img src="banner/below_banner.png" alt="건강한 감정소통커뮤니티, 감정분리수거" />
+</a>
 
 <style lang="scss">
 	img {
